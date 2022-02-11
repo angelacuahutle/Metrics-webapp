522fc70ae4f0446789a35a2054a12bfa8009c210
angelacuahutle <angela.cuahutle@gmail.com>
2022-02-11 17:03:46 -0600

    Add Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,13 @@
-import react from 'react';
+import React from 'react';
 
 function Navbar() {
   return (
-    <About />
+    <nav className="navbar navbar-expand-lg shadow bg-light mb-3">
+      <h1 className="navbar-brand links-nav display-4">Covid Tracker</h1>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+    </nav>
   );
 }
 
