12f59909059c17d72d9a218502a4715809d583d9
angelacuahutle <angela.cuahutle@gmail.com>
2022-02-11 14:22:24 -0600

    Add some design elements

--- src/Components/Continent.js
+++ src/Components/Continent.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import { getContinents } from '../Redux/ReducerContinent';
 
 const Continents = () => {
@@ -13,28 +15,26 @@ const Continents = () => {
   }, [dispatch]);
   if (continents.continent !== undefined) {
     return (
-      <div className="container">
+      <Container className="container">
         {loading ? (
-          <div className="d-flex justify-content-center mt-5">
+          <div className="d-none">
             <span className="mt-5 h3">Loading...</span>
           </div>
         ) : (
-          <div className="d-flex row">
-            {continents.continent.map((continent) => (
-              <div key={continent.id}>
-                <div className="grid">
-                  <div className="col-6 bg-warning border h-100">
-                    {continent.name}{continent.image}
+          <Row>{continents.continent.map((continent) => (
+            
+            <div key={continent.id}></Row>
+             <div className="col-6 bg-warning border h-100">
+               {continent.name}
+                {continent.image}
                     <Link to={`/CountryDetail/${continent.id}`} className="m-2">
                       <p className="d-none">{continent.name}</p>
                     </Link>
                   </div>
                 </div>
-              </div>
             ))}
-          </div>
         )}
-      </div>
+      </Container>
     );
   }
 
